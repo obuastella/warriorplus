dfbf3d2de60b06fb5da74516a510ba0a177f64f5
Obua Stella <stellaobua123@gmail.com>
2025-04-09 23:41:31 +0100

    wip:dashboard

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,6 +1,11 @@
+import Header from "./components/Header";
+import Statistics from "./components/Statistics";
 
 export default function Dashboard() {
   return (
-    <div>Dashboard</div>
-  )
+    <div className="space-y-8">
+      <Header />
+      <Statistics />
+    </div>
+  );
 }
---
+++ src/pages/Dashboard/components/Header.tsx
@@ -0,0 +1,18 @@
+import { Hand } from "lucide-react";
+
+export default function Header() {
+  return (
+    <section className="p-8 flex justify-start items-start gap-x-4 w-full bg-primary text-white">
+      <div>
+        <Hand size={40} className="animate-pulse" />
+      </div>
+      <div>
+        <h1 className="text-lg font-bold">Hello, User!</h1>
+        <p className="text-lg">
+          Today is Monday. Remeber to stay hydrated and take your medication.
+          Stay strong!
+        </p>
+      </div>
+    </section>
+  );
+}
