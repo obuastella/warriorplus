58fccb6ba6768c41905b6ffa318d4860b5c08b4e
Obua Stella <stellaobua123@gmail.com>
2025-04-27 21:16:32 +0100

    Resolving vercel issues and updated verify email ui

--- src/pages/(Auth)/VerifyEmail/VerifyEmail.tsx
+++ src/pages/(Auth)/VerifyEmail/VerifyEmail.tsx
@@ -2,8 +2,7 @@ import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "../../../components/firebase";
-import Lottie from "lottie-react";
-import communityAnimation from "../../../lotties/community.json";
+import { CheckCircle, Mail } from "lucide-react";
 
 export default function VerifyEmail() {
   const navigate = useNavigate();
@@ -19,18 +18,34 @@ export default function VerifyEmail() {
   }, [navigate]);
 
   return (
-    <div className="w-full gap-y-4 flex flex-col justify-center items-center">
-      <div className="rounded-md mt-16 hidden lg:flex flex-col justify-center items-center bg-primary/90 w-[35%] h-full">
-        <Lottie animationData={communityAnimation} loop className="w-[80%]" />
-      </div>
-      <div>
-        <h1 className="text-center text-4xl font-semibold">
-          Verify your Email
-        </h1>
-        <p className="text-center text-gray-400">
-          We've sent a verification email to your inbox. Please verify your
-          email before logging in.
-        </p>
+    <div className="flex min-h-screen w-full bg-gradient-to-b from-primary-50 to-primary/100">
+      <div className="w-full  flex items-center justify-center p-6">
+        <div className="bg-primary/80 rounded-2xl shadow-lg p-8 max-w-md  w-full">
+          <div className="flex justify-center  mb-6">
+            <div className="p-4 rounded-full">
+              <Mail className="h-12 w-12 text-primary" />
+            </div>
+          </div>
+
+          <h1 className="text-center text-3xl font-bold mb-3 text-white">
+            Verify your Email
+          </h1>
+
+          <p className="text-center text-gray-900 text-sm mb-6">
+            We've sent a verification email, Please check your inbox to verify
+            your account.
+          </p>
+
+          <div className="bg-blue-900/20 rounded-lg p-4 mb-6">
+            <div className="flex items-start">
+              <CheckCircle className="h-5 w-5 text-blue-500 dark:text-blue-400 mt-0.5 mr-2 flex-shrink-0" />
+              <p className="text-sm text-blue-700 dark:text-blue-300">
+                Make sure to check your spam folder if you don't see the email
+                in your inbox.
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
